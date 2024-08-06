41fa873a6f1619f4141e35b6a846aea7d14e03e4
joy208das <158313870+joy208das@users.noreply.github.com>
2024-08-06 17:34:29 +0600

    Add files via upload

---
+++ 47oop-turtle.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+print(timmy)
+
+my_screen = Screen()  #my_screen,car these are object; Screen(),carblueprint() these are class
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+
+print(my_screen.canvheight)  #canvheight,color these are atributes
+my_screen.exitonclick()
+
+"""search python turtle graphics 
+you can find all libraries about it"""
